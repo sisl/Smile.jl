e849938eb26a97d95eb9e24be28fc28b331d36d6
Tim Wheeler <wheelert@stanford.edu>
2014-09-23 17:12:41 -0700

    fixed library locator

--- src/Smile.jl
+++ src/Smile.jl
@@ -13,30 +13,22 @@ module Smile
 ##
 ##############################################################################
 
-try
-	ccall( (:libexists, "libsmilejl"), Bool, () )
-catch
+if isempty(find_library(["libsmilejl"]))
 	warn("Could not communicate with libsmilejl. Make sure it exists and that it is on the path.")
-	return
+else
+
+	include("types.jl")
+	include("network.jl")
+	include("utils.jl")
+	include("node.jl")
+	include("nodedefinition.jl")
+	include("idarray.jl")
+	include("doublearray.jl")
+	include("syscoordinates.jl")
+	include("dataset.jl")
+	include("nodevalue.jl")
+	include("dmatrix.jl")
+
 end
-
-
-##############################################################################
-##
-## Load files
-##
-##############################################################################
-
-include("types.jl")
-include("network.jl")
-include("utils.jl")
-include("node.jl")
-include("nodedefinition.jl")
-include("idarray.jl")
-include("doublearray.jl")
-include("syscoordinates.jl")
-include("dataset.jl")
-include("nodevalue.jl")
-include("dmatrix.jl")
 
 end
