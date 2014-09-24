15de59f615feb4aff80d55bf8b10d58e030fa3d4
Tim Wheeler <wheelert@stanford.edu>
2014-09-23 17:00:16 -0700

    erasing build.jl for now...

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,11 @@
-using BinDeps
-@BinDeps.setup
-
-deps = [
-	libsmilejl = library_dependency("smilejl", aliases=["libsmilejl", "libsmilejl.so.1.0", "libsmilejl.so.1", "libsmilejl.so"], os=:Unix)
-]
-
-@BinDeps.if_install begin
-
-provides(Binaries, {URI("http://dl.bintray.com/tawheeler/generic/libsmilejl.so/libsmilejl.so") => deps}, os = :Unix)
+# using BinDeps
+# @BinDeps.setup
 
 
-@BinDeps.install
+# libsmilejl = library_dependency("libsmilejl.so", os=:Unix)
+
+# provides(Binaries, 
+# 	URI("http://dl.bintray.com/tawheeler/generic/libsmilejl.so/libsmilejl.so"), 
+# 	libsmilejl, os = :Unix)
+
+# @BinDeps.install [:libsmilejl => :libsmilejl]
