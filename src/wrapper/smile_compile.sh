89709e945780ad2d55484e3b6efef1000379316c
Tim Wheeler <wheelert@stanford.edu>
2014-10-14 14:34:12 -0700

    cleaned

--- src/wrapper/smile_compile.sh
+++ src/wrapper/smile_compile.sh
@@ -1,13 +1,10 @@
 # RUN THIS TO COMPILE THE LIBRARY "bash -x ./smile_compile.sh"
 
+# compile wrapper
 g++ -Wall -fPIC -c smile_wrapper.cpp -L./lib -lsmile
+# create shared library
 gcc -shared -o libsmilejl.so -Wl,--whole-archive libsmile.a libsmilearn.a -Wl,--no-whole-archive -Wl,-soname,libsmilejl.so.1 -o libsmilejl.so.1.0  smile_wrapper.o
-
-
+# delete any existing shared library
 rm ../../deps/downloads/libsmilejl.so
+# move shared library over
 mv libsmilejl.so.1.0 ../../deps/downloads/libsmilejl.so
-
-# sudo rm downloads/libsmilejl*
-# sudo mv libsmilejl.so.1.0 /usr/lib/
-# sudo ln -sf /usr/lib/libsmilejl.so.1.0 /usr/lib/libsmilejl.so.1
-# sudo ln -sf /usr/lib/libsmilejl.so.1.0 /usr/lib/libsmilejl.so
